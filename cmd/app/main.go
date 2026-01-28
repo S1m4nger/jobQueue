0865037008e7d5174f1d9fc526478ff502c18677
vova vakhtin <vovavahtin15@gmail.com>
2026-01-29 01:14:33 +0500

    some fixes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -14,16 +14,24 @@ import (
 	"syscall"
 	"time"
 
+	_ "modernc.org/sqlite"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// 1. Database setup (GORM + SQLite)
-	gdb, err := gorm.Open(sqlite.Open("./jobs.db"), &gorm.Config{})
+	// 1. Database setup (GORM + SQLite, CGO-free)
+	gdb, err := gorm.Open(
+		sqlite.Open("./jobs.db?_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)"),
+		&gorm.Config{
+			DisableForeignKeyConstraintWhenMigrating: true,
+		},
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	sqlDB, err := gdb.DB()
 	if err != nil {
 		log.Fatal(err)
@@ -42,7 +50,7 @@ func main() {
 	defer cancel()
 	svc.StartWorkers(ctx, 3)
 
-	// 4. Start Server
+	// 4. HTTP server
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
@@ -51,7 +59,7 @@ func main() {
 	go func() {
 		log.Println("Starting server on :8080")
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			log.Fatalf("listen error: %s", err)
 		}
 	}()
 
@@ -59,9 +67,11 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
+
 	log.Println("Shutting down server...")
 
-	cancel() // Stop workers
+	// stop workers
+	cancel()
 
 	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdownCancel()
@@ -70,5 +80,5 @@ func main() {
 		log.Fatal("Server forced to shutdown:", err)
 	}
 
-	log.Println("Server exiting")
+	log.Println("Server exited cleanly")
 }
